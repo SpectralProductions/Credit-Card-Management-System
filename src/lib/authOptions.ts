d7b0c53d1863510422d32284b395480585861aa7
SpectralProductions <110586132+SpectralProductions@users.noreply.github.com>
2025-06-20 14:20:52 +0530

    save6

---
+++ src/lib/authOptions.ts
@@ -0,0 +1,25 @@
+// src/lib/authOptions.ts
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "prisma";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // your login logic
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
